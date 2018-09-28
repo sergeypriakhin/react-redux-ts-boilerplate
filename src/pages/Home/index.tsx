cabb6165e87832065e25f92f3e46825c97516cb1
chucho <chucho@syneforge.com>
2018-09-28 17:10:00 +0300

    add antd

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import * as s from './Home.css';
-import LinkA from '../../components/Link';
 
 const Home = () => {
   return (
@@ -9,18 +7,6 @@ const Home = () => {
       <Helmet>
         <title>Home page</title>
       </Helmet>
-      <section className={s.root}>
-        <div>
-          <h1 className={s.header1}>
-            Quick setup for new performance orientated, offline–first React.js
-            applications
-          </h1>
-          <LinkA
-            link="https://github.com/sergeypriakhin/react-redux-ts-boilerplate"
-            title="Source"
-          />
-        </div>
-      </section>
     </React.Fragment>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const tsImportPluginFactory = require('ts-import-plugin');
 
 module.exports = function(env, argv) {
   const isDev = argv.mode === 'development';
@@ -33,17 +34,38 @@ module.exports = function(env, argv) {
 
     module: {
       rules: [
-        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
         {
           test: /\.tsx?$/,
           loader: 'awesome-typescript-loader',
+          options: {
+            getCustomTransformers: () => ({
+              before: [
+                tsImportPluginFactory({
+                  libraryName: 'antd',
+                  libraryDirectory: 'lib',
+                  style: 'css',
+                }),
+              ],
+            }),
+          },
           exclude: /node_modules/,
+        },
+
+        {
+          // Preprocess our own .css files
+          // This is the place to add your own loaders (e.g. sass/less etc.)
+          // for a list of loaders, see https://webpack.js.org/loaders/#styling
+          test: /\.css$/,
+          include: /node_modules/,
+          use: ['style-loader', 'css-loader'],
         },
 
         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
         { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
         {
           test: /\.(sa|sc|c)ss$/,
+          include: /src/,
+          exclude: /node_modules/,
           loaders: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
